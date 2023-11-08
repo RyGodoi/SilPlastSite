e69c85eba39007eccb03fd04ed7e8cb9998e464c
Ryan Godoi da Silva <Ryan.Godoi.daSilva@gmail.com>
2023-11-08 10:10:19 -0300

    adicionando pedidos e galeria.

--- js/init.js
+++ js/init.js
@@ -79,4 +79,60 @@
   });
   //navbar
 
+  //pedidos
+  var arraymedidas = [];
+  var arraywhatsapp = [];
+  $("#adicionar").on("click", function (e) {
+    e.preventDefault;
+
+    var larg = $("#largura").val();
+    var alt = $("#altura").val();
+    var espess = $("#espessura").val();
+    var quant = $("#quantidade").val();
+
+    if (larg != "" && alt != "" && espess != "" && quant != "" && nome != "") {
+      $("#div-lista").append("<li><p>Pedido " + arraymedidas.length + ": " + larg + "(cm) <b>X</b> " + alt + "(cm) <b>X</b> " + espess + "(mm) <b>=</b> " + quant + "(kg)</p></li>");
+      var medidasString = "Pedido " + arraymedidas.length + ": " + larg.toString() + "(cm) X " + alt.toString() + "(cm) X " + espess.toString() + "(mm) = " + quant + "(kg)";
+      arraymedidas.push(medidasString);
+      $('input[type="number"]').val('');
+
+    } else {
+      alert("Você deixou de preencher alguma informação!");
+
+
+    }
+    console.log(medidasString);
+    console.log(arraymedidas);
+    return false;
+  })
+
+  $("#apagar").on("click", function () {
+    if (arraymedidas != "") {
+      arraymedidas.pop();
+      console.log(arraymedidas);
+      $('#div-lista li:last-child').remove();
+
+    } else {
+      alert("Não há nenhuma medida na sua lista.")
+    }
+  })
+
+  $("#gerar-link").on("click", function () {
+    if (arraymedidas.length != 0) {
+
+      for (let index = 0; index < arraymedidas.length; index++) {
+        arraywhatsapp.push(arraymedidas[index].replace(/ /g, "%20") + "%0A%0A");
+      }
+
+      console.log(arraywhatsapp);
+
+
+      $(".links-gerados a").remove();
+      $(".links-gerados").append(
+        "<a href='https://api.whatsapp.com/send?phone=5511953760769&text=Ol%C3%A1%20Silplast,%20esses%20s%C3%A3o%20meus%20pedidos:%0A%0A" + arraymedidas + "%0A%0Amuito%20obrigado%20pela%20aten%C3%A7%C3%A3o,%20aguardo%20retorno.'>Click aqui para encaminhar o pedido</a>");
+
+
+    }
+  })
+
 })(jQuery); // end of jQuery name space
